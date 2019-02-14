1a6ef1d09932bba148fb1b7b5ed1fd1ae173a000
annadowling <anna.dowling@errigal.com>
2019-02-14 12:04:05 +0000

    Added correlation functions

--- task1/Correlation.py
+++ task1/Correlation.py
@@ -1,28 +1,34 @@
+# Importing required libraries
 import pandas as pd
-import numpy as np
-'exec(%matplotlib inline)'
 import matplotlib.pyplot as plt
-import seaborn as sn
 from scipy import stats
 
 
-# Normality	of	X	and	Y;
-# “Linear”	dependency;
-# Homogeneity	of	Variance:
-# Homoscedasticity; Levene’s	test:	scipy.stats.levene(…)
+carData = pd.read_csv('../data/cleaned-auto-mpg.csv')
 
 
-carData = pd.read_csv('../data/regression_lab.csv')
+def check_assumptions(x, y, data):
+    print(data[[x, y]].describe())
+
+    # Checking the assumptions
+    data.plot.scatter(x, y)
+    plt.show()
+    # Homogeneity of Variance
+    print(stats.levene(data[x], data[y]))
 
 
-def perform_correlation(col1, col2):
-    pearson_coef, p_value = stats.pearsonr(col1, col2)
-    print("Pearson Correlation Coefficient: ", pearson_coef, "and a P-value of:", p_value)
+def check_correlation(x, y, data):
+    # Pearson correlation
+    print(stats.pearsonr(data[x], data[y]))
+
+    # Pandas correlation of two columns
+    print(data[x].corr(data[y]))
 
 
-perform_correlation(carData['mpg'], carData['acceleration'])
-perform_correlation(carData['mpg'], carData['weight'])
-perform_correlation(carData['mpg'], carData['displacement'])
-horsepower = pd.to_numeric(carData["horsepower"], errors='coerce')
-horsepower.dropna()
-perform_correlation(carData['mpg'], horsepower)
+check_assumptions("mpg", "weight", carData)
+check_correlation("mpg", "weight", carData)
+
+
+
+
+
