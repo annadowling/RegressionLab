6d9cf76a63f12451aef7f6beff4a82e79adc9dea
annadowling <anna.dowling@errigal.com>
2019-02-19 13:13:33 +0000

    Work on categorical correlation

---
+++ task1/CategoryCorrelation.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import scipy.stats as scs
+
+def chi_square_of_df_cols(df, col1, col2):
+    return scs.chi2_contingency(pd.crosstab(df[col2], df[col1]))
+
+
+def TestIndependence(df ,colX ,colY):
+    X = df[colX].astype(str)
+    Y = df[colY].astype(str)
+
+    dfObserved = pd.crosstab(Y,X)
+    chi2, p, dof, expected = scs.chi2_contingency(dfObserved.values)
+    print("chi2")
+    print(chi2)
+    print("p-value")
+    print(p) # p-value
+    print("Degree of Freedom") # The number of independent variates which make up the statistic (eg chi-square) is known as degree of freedom of that statistic.
+    print(dof)
+    print("Expected Array")
+    print(expected) # expected array
+
+    dfExpected = pd.DataFrame(expected, columns=dfObserved.columns, index=dfObserved.index)
+
+carData = pd.read_csv('../data/cleaned-outliers-auto-mpg.csv')
+
+TestIndependence(carData, 'model year', 'cylinders')
+TestIndependence(carData, 'Europe', 'cylinders')
+TestIndependence(carData, 'Asia', 'cylinders')
+TestIndependence(carData, 'USA', 'cylinders')
+
+
+
